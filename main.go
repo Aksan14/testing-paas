eac02e8b530c2709ceb911b86dd58a33d865581f
Aksan14 <105841107223@student.unismuh.ac.id>
2026-05-02 18:36:08 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from Go Server")
+	})
+
+	fmt.Println("Server jalan di http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
